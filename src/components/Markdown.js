fcaec1988d55e0f6c4371f40c35317400f343c46
Eamon Taaffe <eamontaaffe@gmail.com>
2017-05-07 17:37:34 +1000

    Improvement to blog formatting using highlight.js

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import marked from 'marked';
+import highlightjs from 'highlight.js';
+
+import 'highlight.js/styles/atom-one-light.css'
+import marked, { Renderer } from 'marked';
+
+// Create your custom renderer.
+const renderer = new Renderer();
+renderer.code = (code, language) => {
+    // Check whether the given language is valid for highlight.js.
+    const validLang = !!(language && highlightjs.getLanguage(language));
+    // Highlight only if the language is valid.
+    const highlighted = validLang ? highlightjs.highlight(language, code).value : code;
+    // Render the highlighted code with `hljs` class.
+    return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
+};
 
 const Markdown = ({content}) => {
     // Synchronous highlighting with highlight.js
-    marked.setOptions({
-        highlight: function (code) {
-            return require('highlight.js').highlightAuto(code).value;
-        }
-    });
+    marked.setOptions({ renderer });
     
     const inner = {__html: marked(content)} 
     return (
