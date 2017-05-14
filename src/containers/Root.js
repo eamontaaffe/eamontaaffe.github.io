f10faa688e1f6ba2f862a514ac8bca0ed4eaf293
Eamon Taaffe <eamontaaffe@gmail.com>
2017-05-14 21:12:04 +1000

    Upgraded react router to v4

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Router, Route, hashHistory } from 'react-router';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import configureStore from '../configureStore';
 
 import MainLayout from './MainLayout'
@@ -9,19 +9,23 @@ import Blog from './Blog';
 import AboutContainer from './AboutContainer';
 import Books from './Books';
 import Podcasts from './Podcasts';
+import NoMatch from './NoMatch';
 
 const store = configureStore();
 
 const Root = () => (
 	<Provider store={store}>
-	<Router history={hashHistory}>
-	<Route component={MainLayout}>
-	<Route path="/" component={App} />
+	<Router>
+        <MainLayout>
+        <Switch>
+	<Route exact path="/" component={App} />
 	<Route path="/blog" component={Blog} />
 	<Route path="/about" component={AboutContainer} />
 	<Route path="/books" component={Books} />
 	<Route path="/podcasts" component={Podcasts} />
-	</Route>
+        <Route component={NoMatch} />
+        </Switch>
+        </MainLayout>
 	</Router>
 	</Provider>
 )
