44fcbfa5d7513f731033e0e201ca941a42d90c95
Eamon Taaffe <eamontaaffe@gmail.com>
2018-08-14 20:06:04 +1000

    Remove all the bloat, just keep it simple

--- src/Site.hs
+++ src/Site.hs
@@ -4,13 +4,9 @@
 module Site (run) where
 
 
-import           Hakyll
-import           Hakyll.Web.Pandoc
-import           Data.Maybe (fromJust)
-
-import           Aggregate (buildEvents, compileAggregate)
-import qualified About as A
-import qualified Books as B
+import Hakyll
+import Hakyll.Web.Pandoc
+import Data.Monoid ((<>))
 
 -- Exposed
 --------------------------------------------------------------------------------
@@ -20,51 +16,33 @@ run = hakyll $ do
   match "templates/*" $
     compile templateBodyCompiler
 
-  match "events/*.md" $ do
-    compile $ pandocCompiler
-      >>= saveSnapshot "events"
-
-  events <-
-    buildEvents "events/*"
-
   create ["pages/index.md"] $ do
     route $ constRoute "index.html"
     compile $ pandocCompiler
       >>= loadAndApplyTemplate "templates/default.html" defaultContext
 
+  match "events/*.about.md" $ do
+    compile $ pandocCompiler
+      >>= saveSnapshot "about"
+
   create ["about.html"] $ do
     route idRoute
     compile $ do
-      about@(A.State{A.aboutBodyId=b, A.edits=e}) <-
-        compileAggregate A.aggregate events
-
-      body <- loadSnapshotBody (fromJust b) "events" :: Compiler (String)
-
-      let aboutCtx =
-            constField "edits" (show e) `mappend`
-            bodyField "about"
-
-      let defaultCtx =
-            constField "title" "About" `mappend`
-            bodyField "body"
-
-      makeItem body
-        >>= loadAndApplyTemplate "templates/about.html" aboutCtx
-        >>= loadAndApplyTemplate "templates/default.html" defaultCtx
-
-  create ["books.html"] $ do
-    route idRoute
-    compile $ do
-      books <- compileAggregate B.aggregate events
-
-      let booksCtx =
-            constField "total" (show $ length books) `mappend`
-            listFieldWith "books" defaultContext (return books)
-
-      let defaultCtx =
-            constField "title" "Books" `mappend`
-            bodyField "body"
-
+      abouts <-
+        loadAllSnapshots "events/*.about.md" "about" :: Compiler [Item String]
+      let ctx =
+            aboutCtx abouts
       makeItem ""
-        >>= loadAndApplyTemplate "templates/books.html" booksCtx
-        >>= loadAndApplyTemplate "templates/default.html" defaultCtx
+        >>= loadAndApplyTemplate "templates/about.html" ctx
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+
+
+-- Internal
+--------------------------------------------------------------------------------
+
+aboutCtx :: [Item String] -> Context String
+aboutCtx abouts
+  =  constField "title" "About"
+  <> constField "about" (itemBody . last $  abouts)
+  <> constField "edits" (show . ((-) 1) . length $ abouts)
+  <> defaultContext
