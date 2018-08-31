c57762230e31e836eee1947e76b73ebde260c52d
Eamon Taaffe <eamontaaffe@gmail.com>
2018-08-31 14:52:51 +1000

    Highlight corresponding country on hover

---
+++ js/travel.js
@@ -0,0 +1,106 @@
+var forEachElement = function (els, fn) {
+  for (var i = 0; i < els.length; i++) {
+    fn(els.item(i));
+  }
+};
+
+/******************************************************************************/
+
+var findCountryThen = function (code, fn) {
+  var el = document.getElementById("country-" + code);
+  if (el) {
+    return fn (el);
+  }
+  return null;
+};
+
+var onLandMouseover = function (event) {
+  return findCountryThen(
+    event.target.id,
+    function (el) {
+      return el.className += " active";
+    },
+  );
+    
+};
+
+var onLandMouseout = function (event) {
+  return findCountryThen(
+    event.target.id,
+    function(el) {
+      el.className = el.className.replace(" active", ""); 
+    }
+  );
+};
+
+var landUpdate = function (el) {
+  el.addEventListener(
+    "mouseover",
+    onLandMouseover,
+  );
+
+  el.addEventListener(
+    "mouseout",
+    onLandMouseout,
+  );
+};
+
+var lands = document.getElementsByClassName("land");
+
+forEachElement(lands, landUpdate);
+
+/******************************************************************************/
+
+var getCountryThen = function (code, fn) {
+  var el = document.getElementById(code);
+
+  if (el) {
+    return fn(el);
+  }
+
+  return null;
+};
+
+var onCountryMouseover = function (event) {
+  var code = event
+      .target
+      .id
+      .replace("country-", "");
+
+  return getCountryThen(
+    code,
+    function (el) { el.className.baseVal += " active"; },
+  );
+};
+
+var onCountryMouseout = function (event) {
+  var code = event
+      .target
+      .id
+      .replace("country-", "");
+
+  return getCountryThen(
+    code,
+    function (el) {
+      el.className.baseVal = el.className.baseVal.replace(" active", "");
+    },
+  );
+};
+
+var countryUpdate = function (el) {
+  el.addEventListener(
+    "mouseover",
+    onCountryMouseover,
+  );
+
+  el.addEventListener(
+    "mouseout",
+    onCountryMouseout,
+  );
+};
+
+var countries = document.getElementsByClassName("country");
+
+forEachElement(countries, countryUpdate);
+
+
